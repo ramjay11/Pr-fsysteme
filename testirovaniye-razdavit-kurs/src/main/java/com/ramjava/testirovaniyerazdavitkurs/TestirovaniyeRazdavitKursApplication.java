10ca05abd8ac3814fb4ecbbecc935430769c713a
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2024-01-14 04:33:56 +0800

    первоначальный коммит

---
+++ testirovaniye-razdavit-kurs/src/main/java/com/ramjava/testirovaniyerazdavitkurs/TestirovaniyeRazdavitKursApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.testirovaniyerazdavitkurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestirovaniyeRazdavitKursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestirovaniyeRazdavitKursApplication.class, args);
+	}
+
+}
